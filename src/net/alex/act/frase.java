3e5fe33015f01644b55e480738697c5a84bed8f0
alix <halohalex@gmail.com>
2013-10-27 02:26:28 +0200

    he afegit hola.txt i la paraula PAZ

--- src/net/alex/act/frase.java
+++ src/net/alex/act/frase.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class frase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub PAZ
 		
 		
 		Scanner lector= new Scanner(System.in);
